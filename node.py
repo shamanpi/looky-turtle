3568bba7eb86418e6f3696258a4c0e7586d136f9
Sam Hamann <haman152@umn.edu>
2017-11-22 00:38:06 -0600

    Added comments to node.py, also trimmed some excess code and laid out plans

--- node.py
+++ node.py
@@ -1,17 +1,21 @@
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
-
 import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
-# frame x = uint32 seq from the data, used for timing
 p = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
+bridge = CvBridge()
 
+# callback function is called whenever the subscriber detects new data within the camera
+# topic, takes the topic's data as input, and uses that for whatever purpose we define.
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id())
-    p.publish(Twist(Vector3(data[0][0] / 255, data[0][1] / 255, data[0][2] / 255), Vector3(0, 0, 0)))
+    try:
+	cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+	print(e)
+    (rows, cols, channels) = cv_image.shape
 
 if __name__ == '__main__':
   rospy.Subscriber('camera/rgb/image_raw', Image, callback)
@@ -19,7 +23,17 @@ if __name__ == '__main__':
   rate = rospy.Rate(10)
   while not rospy.is_shutdown():
     str = rospy.get_time()
-#    rospy.loginfo("%s" % str)
     img = rospy
-    p.publish("%s" % str)
+    p.publish(Twist(Vector3(frame/1000,frame/1000,frame/1000), Vector3(0,0,0)))
+#    rospy.loginfo("%s" % str)
     rate.sleep()
+
+
+# Overall plan: import necessary data and message libraries, including cv2 and cv_bridge.
+# Define the necessary callback function, publisher, subscriber, and CvBridge objects.
+# Define other necessary functions for the ML Object Detection code from Kent.
+# Main Method Order:
+#   1. Take video feed as input with callback function
+#   2. Check if current frame is xth frame since last analyzed: If not, change nothing.
+#   3. Analyze frame to detect object and its position within the image frames.
+#   4. Adjust heading and velocity based on said object's position: Can stop if need be
