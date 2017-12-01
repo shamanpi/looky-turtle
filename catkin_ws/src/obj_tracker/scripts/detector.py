78d396a7eb55b1bf7fd90ae0f719328df9d072a9
Samuel Hamann <haman152@umn.edu>
2017-12-01 12:41:44 -0600

    Checkpoint pre-testing

--- catkin_ws/src/obj_tracker/scripts/detector.py
+++ catkin_ws/src/obj_tracker/scripts/detector.py
@@ -12,7 +12,7 @@ cap = cv2.VideoCapture(0)
 def detector():
     pub = rospy.Publisher('detector_topic', String, queue_size=10)
     rospy.init_node('detector_node', anonymous=True)
-    rate = rospy.Rate(3) #3 Hz
+    rate = rospy.Rate(100) #3 Hz
     while not rospy.is_shutdown():
 
         ret, img = cap.read()
@@ -20,13 +20,19 @@ def detector():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+#	 Only functions correctly if uses cv2.waitKey(1) & 0xFF == ord(key)?
+        cv2.imshow('img', img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+	  break
+
         for (x,y,w,h) in faces:
-            coords = x + "," + y + "," + w + "," + h
+            coords = `x` + "," + `y` + "," + `w` + "," + `h`
             pub.publish(coords)
 
-        cv2.imshow('img', img)
-
-        rospy.loginfo(coords)
+        if (faces != ()):
+            rospy.loginfo(coords)
+        else:
+            rospy.loginfo("None found")
         rate.sleep()
 
 if __name__ == '__main__':
